316c882e8c6699b24ee827e55de4e9250fff5396
Luis Rosa <18100115@ue.edu.pe>
2022-06-04 08:47:18 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cp202201_voto_MVC_v1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
