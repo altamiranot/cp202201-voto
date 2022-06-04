8217712e13c4e95b716a67d147aa6433025a5c69
Luis Rosa <18100115@ue.edu.pe>
2022-06-04 09:05:42 -0500

    Creacion del API

---
+++ Controllers/PruebaAPI.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cp202201_voto_API_v1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PruebaAPI : ControllerBase
+    {
+    }
+}
