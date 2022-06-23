289c245c4b2a63766a4d826c4fc35549dfa54ef9
Oscar Eduardo Salazar García <edu082000@hotmail.com>
2022-06-23 17:52:16 -0500

    Controlador Security

---
+++ Controllers/SecurityController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cp202201_voto_MVC_v1.Controllers
+{
+    public class SecurityController : Controller
+    {
+        public IActionResult signUp()
+        {
+            return View();
+        }
+    }
+}
