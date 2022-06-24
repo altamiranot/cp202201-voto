7d3a64f775dd9039a85363f0baa888a281c99f2e
Oscar Eduardo Salazar García <edu082000@hotmail.com>
2022-06-23 19:56:50 -0500

    ViewPrincipal

---
+++ Controllers/VoterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cp202201_voto_MVC_v1.Controllers
+{
+    public class VoterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
