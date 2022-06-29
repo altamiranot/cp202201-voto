004c1b1c229078affc8d7b554e3277d33f45ae22
Oscar Eduardo Salazar García <edu082000@hotmail.com>
2022-06-29 17:43:47 -0500

    Javascript

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,5 +28,6 @@ namespace cp202201_voto_MVC_v1.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        
     }
 }
