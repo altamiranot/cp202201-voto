7678dae8e840479d8ea46ca4e71236798e826ea9
MiguelGr213 <Miguel789.gr21@gmail.com>
2022-07-07 23:31:39 -0500

    Models y Service

---
+++ Models/UsuarioModel.cs
@@ -0,0 +1,17 @@
+﻿namespace cp202201_voto_MVC_v1.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string? Paterno { get; set; }
+        public string? Materno { get; set; }
+        public string? Nombres { get; set; }
+        public string? NumeroDocumento { get; set; }
+        public string? Telefono { get; set; }
+        public string? Dirección { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+        public string? Password { get; set; }
+        public string? Correo { get; set; }
+        public bool? EsAdmin { get; set; }
+    }
+}
---
+++ Service/UsuarioService.cs
@@ -0,0 +1,22 @@
+﻿using cp202201_voto_MVC_v1.Models;
+using Newtonsoft.Json;
+
+namespace cp202201_voto_MVC_v1.Service
+{
+    public class UsuarioService
+    {
+        public static async Task<UsuarioModel> Post()
+        { 
+            string url = "http://localhost:5158/api/Usuario/Create";
+
+            var client = new HttpClient();
+            var response = await client.PostAsync(url + "Create", null);
+
+            string result = await response.Content.ReadAsStringAsync();
+
+            var usuarios = JsonConvert.DeserializeObject<UsuarioModel>(result);
+
+            return usuarios;
+        }
+    }
+}
